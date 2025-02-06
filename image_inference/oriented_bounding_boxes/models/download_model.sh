0999b5eed70e6bccebf69171676c2f6e1d0f353a
Tim <tim@memryx.com>
2025-02-06 12:42:37 -0500

    add missing download_model.sh for yolo-OBB

---
+++ image_inference/oriented_bounding_boxes/models/download_model.sh
@@ -0,0 +1,8 @@
+
+name="YOLO_v8_small_Oriented_Bounding_Boxes_1024_1024_3_onnx"
+wget https://developer.memryx.com/model_explorer/1p1/${name}.zip
+unzip ${name}.zip -d models
+mv models/${name}.dfp models/yolov8s-obb.dfp
+mv models/${name}_post.onnx models/yolov8s-obb_post.onnx
+rm ${name}.zip;
+
